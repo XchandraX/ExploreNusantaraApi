90651b6b7f75516fc84cb630d10f171d8b793986
chandrx <146406701+chandrx@users.noreply.github.com>
2025-04-01 13:56:34 +0700

    Menambahkan API endpoint untuk login pengguna

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 9000, // Gunakan PORT dari environment atau default ke 9000
+    port: 9000, // Gunakan PORT dari environment atau default ke 9000
     host: "0.0.0.0",  // Gunakan 0.0.0.0 agar dapat diakses dari luar container/host
     routes: { cors: { origin: ["*"] } },
   });
